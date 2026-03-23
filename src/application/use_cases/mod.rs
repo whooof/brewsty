e1cfe4ee77ae62d907c54159df567f105a802958
Jan Konieczny <whooof@outlook.com>
2026-03-23 12:16:34 +0100

    feat(3.3): add Import Packages use case with JSON/YAML support

--- src/application/use_cases/mod.rs
+++ src/application/use_cases/mod.rs
@@ -1,6 +1,7 @@
 pub mod check_updates;
 pub mod export_packages;
 pub mod history_operations;
+pub mod import_packages;
 pub mod package_details;
 pub mod package_list_operations;
 pub mod package_operations;
@@ -9,6 +10,7 @@ pub mod service_operations;
 pub use check_updates::*;
 pub use export_packages::*;
 pub use history_operations::*;
+pub use import_packages::*;
 pub use package_details::*;
 pub use package_list_operations::*;
 pub use package_operations::*;
